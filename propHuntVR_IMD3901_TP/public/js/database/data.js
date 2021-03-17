8b1c5dc50d7e4e9c1e64bbad3295ba34ccce485b
TomasBabkine-DiCaprio <60266939+TomasBabkine-DiCaprio@users.noreply.github.com>
2021-03-17 19:55:03 -0400

    Create data.js

---
+++ propHuntVR_IMD3901_TP/public/js/database/data.js
@@ -0,0 +1,16 @@
+exhibitItems = {
+    'example1' : {
+        'id' : '',
+        'name' : '',
+        'parts' : ['', '', ''],
+        'pathTo3DAsset' : '../../assets/',
+        'pathToImage' : '../../assets/images'
+    },  // End of Example 1
+    'example2' : {
+        'id' : '',
+        'name' : '',
+        'parts' : ['', '', ''],
+        'pathTo3DAsset' : '../../assets/',
+        'pathToImage' : '../../assets/images'
+    },  // End of Example 2
+}
