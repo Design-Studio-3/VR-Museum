d7e6ec932e80a57e884961b9e1009c3e00cf081c
Mark McGregor <markmcgregor.contact@gmail.com>
2021-03-18 10:01:39 -0400

    Create game-controller component

---
+++ propHuntVR_IMD3901_TP/public/js/components/game-controller.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent('game-controller', {
+  schema: {
+  },
+
+  init: function () {
+    // create self variables
+    var self = this;
+    var el = this.el;
+
+    // set up component variables
+    var currentExhibitItemId = -1;
+    // var
+
+    // add event listeners
+    el.addEventListener('', function () {
+
+    });
+  }
+});
